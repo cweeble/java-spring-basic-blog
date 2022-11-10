41f62035e11c9caad65dbcf311efda94db2b95c7
Andy Chase <anchase@ip-10-137-145-108.eu-west-2.compute.internal>
2022-11-10 16:03:11 +0000

    task 1.4

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,12 +1,13 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
     @RequestMapping("/")
-    public string listposts(ModelMap modelMap) {
+    public String listPosts(ModelMap modelMap) {
         modelMap.put("title", "Blog Post 1");
         return "home";
     }
